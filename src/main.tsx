b6b426e9d673842d8a98a405c633b3a68f086342
bobmberman <adisak.tanant@gmail.com>
2026-03-14 19:04:01 +0700

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Admin from './Admin'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* หน้าแรก (/) สำหรับ CG กรอกข้อมูล */}
+        <Route path="/" element={<App />} />
+        
+        {/* หน้า (/admin) สำหรับ CM จัดการข้อมูล */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
